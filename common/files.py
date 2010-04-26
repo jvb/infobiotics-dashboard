491e624491db7ccac9798fb7de3a920f9dccace8
jvb <devnull@localhost>
2010-04-26 12:31:58 +0100

    Added mkdir_p().

--- common/files.py
+++ common/files.py
@@ -1,6 +1,15 @@
 #__all__ = ['can_access', '']
 
-import os
+import os, errno
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
+
 
 from sequences import join_overlapping
 
