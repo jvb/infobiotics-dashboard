49f7505c1cb819f8cbc5d6a7bc227dec6c550884
jvb <devnull@localhost>
2010-05-02 17:55:21 +0100

    Adding webbrowsing and helpful_controller to commons which should have happened quite a few commits ago!

---
+++ commons/webbrowsing.py
@@ -0,0 +1,15 @@
+from commons.api import which
+import subprocess, webbrowser
+
+def dedicated_window(url):             
+    chrome = which('google-chrome') 
+    if chrome:
+        subprocess.Popen(
+            [chrome,'--app=%s' % url], 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.STDOUT
+        )    
+    else:    
+        webbrowser.open(url, new=1, autoraise=True)
+        # may not open a new browser window depending on users setup
+        
