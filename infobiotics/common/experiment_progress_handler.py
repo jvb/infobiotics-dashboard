6dfe547eb2f394a5155e880f024d4f53bb64bdb7
jvb <devnull@localhost>
2010-05-05 14:29:01 +0100

    Removed redefinition of status trait on experiment_progress_handler. Now to autohide status messages using do_later... so they don't propagate to it (permanently).

--- infobiotics/common/experiment_progress_handler.py
+++ infobiotics/common/experiment_progress_handler.py
@@ -7,18 +7,17 @@ from enthought.traits.ui.api import (
     View, Item, DefaultOverride,                              
 )
 
-class CancelExperimentMixin(object):
-    ''' Mixin '''
-    cancel = Button
-    
-    @on_trait_change('cancel')
-    def cancelled(self):
-        print 'cancelled'
+#class CancelExperimentMixin(object):
+#    ''' Mixin '''
+#    cancel = Button
+#    
+#    @on_trait_change('cancel')
+#    def cancelled(self):
+#        print 'cancelled'
 
 class ExperimentProgressHandler(ParamsHandler):#, CancelExperimentMixin):
     
     progress = Property(Percentage) # subclasses must repeat this line!?
-    status = Property(Str)
     
     def _get_progress(self):
         raise NotImplementedError
@@ -26,9 +25,6 @@ class ExperimentProgressHandler(ParamsHandler):#, CancelExperimentMixin):
     def _set_progress(self): # only need when progress being edited with a RangeEditor
         return
 
-    def _get_status(self):
-        return ''
-        
     traits_view = View(
         Item('controller.progress', editor=DefaultOverride(low_label='0%', high_label='100%', format='%2.1f')),
         Item('controller.status', style='readonly', visible_when='len(controller.status) > 0'),
