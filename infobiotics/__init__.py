6e116f1556dc8e7e2f2df52efb8118b72477b895
jvb <devnull@localhost>
2010-05-05 14:08:51 +0100

    Sets up default _params_program preferences for Infobiotics Workbench components on Windows, if they haven't already been specified.

--- infobiotics/__init__.py
+++ infobiotics/__init__.py
@@ -1,15 +1,32 @@
+''' 
+Sets TraitsUI backend and default_preferences on first import from infobiotics 
+package.
+'''
+
 from enthought.etsconfig.api import ETSConfig 
-
-# set toolkit to 'qt4' (TraitsBackendQt) before any Traits imports
 ETSConfig.toolkit = 'qt4' # os.environ['ETS_TOOLKIT']='qt4' also works
+# set toolkit to 'qt4' (TraitsBackendQt) before any Traits imports
 
-# use ~/.infobiotics instead of ~/.enthought for preferences.ini
 ETSConfig.company = 'infobiotics'
+# use ~/.infobiotics (or "Application Data\infobiotics") instead of 
+# ~/.enthought for preferences.ini
 
-from enthought.preferences.api import set_default_preferences, ScopedPreferences
-import os
-set_default_preferences(
-    ScopedPreferences(
-        filename=os.path.join(ETSConfig.application_data, 'preferences.ini')
-    )
+import os.path
+from enthought.preferences.api import (
+    set_default_preferences, ScopedPreferences, get_default_preferences,
 ) 
+
+preferences_file_name = os.path.join(ETSConfig.application_data, 'preferences.ini') 
+print "Using preferences file '%s'" % preferences_file_name 
+preferences = ScopedPreferences(filename=preferences_file_name)
+
+import platform
+if platform.system() == 'Windows':
+    if preferences.get('mcss._params_program_file', None) is None:
+        preferences.set('mcss._params_program_file', 'C:\\Program Files\\Infobiotics-Workbench\\infobiotics-workbench\\mcss.EXE'),
+    if preferences.get('pmodelchecker._params_program_file', None) is None:
+        preferences.set('pmodelchecker._params_program_file', 'C:\\Program Files\\Infobiotics-Workbench\\infobiotics-workbench\\pmodelchecker.EXE'),
+    if preferences.get('poptimizer._params_program_file', None) is None:
+        preferences.set('poptimizer._params_program_file', 'C:\\Program Files\\Infobiotics-Workbench\\infobiotics-workbench\\poptimizer.EXE'),
+
+set_default_preferences(preferences)
