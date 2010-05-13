421186daa1d10753b1209c6e96bf4d4ab906dc66
jvb <devnull@localhost>
2010-05-13 15:59:21 +0100

    Cross-platform setup.py for use with py2exe and py2app.

--- setup.py
+++ setup.py
@@ -1,40 +1,57 @@
+'''
+distutils install script for Infobiotics Dashboard
+
+source distribution:
+$ python setup.py sdist
+
+Mac app:
+$ python setup.py py2app
+
+Windows executable:
+$ python setup.py py2exe
+
+'''
+
+# bootstrap Distribute
 from distribute_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup, find_packages
 
+# get correct pexpect for platform
+import platform
+if platform.system() == 'Windows':
+    install_requires = [
+#        'winpexpect>=1.3', # doesn't work
+        'http://sage.math.washington.edu/home/goreckc/sage/wexpect/wexpect.py',
+    ]
+else:
+    install_requires = [
+        'pexpect>=2.4',
+    ]
+
+mainscript = 'bin/infobiotics-dashboard.py'
+
+if platform.system() == 'Darwin':
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=[mainscript],
+        # Cross-platform applications generally expect sys.argv to
+        # be used for opening files.
+        options=dict(py2app=dict(argv_emulation=True)),
+    )
+elif platform.system() == 'Windows':
+    extra_options = dict(
+        setup_requires=['py2exe'],
+        app=[mainscript],
+    )
+else:
+    extra_options = dict(
+        scripts=[mainscript],
+    )
+
 setup(
-    
-    packages=find_packages(
-        exclude=[
-            "*.tests", "*.tests.*", "tests.*", "tests", # http://packages.python.org/distribute/setuptools.html#using-find-packages
-            'expect',
-            'thirdparty',
-        ]
-    ),
-
-    scripts=[
-        'bin/infobiotics-dashboard.py',
-    ],
-
-    install_requires=[
-        'EnthoughtBase==3.0.4',
-        'AppTools==3.3.1',
-        'Traits==3.3.0',
-        'EnvisageCore==3.1.2',
-        'TraitsGUI==3.3.0',
-        'EnvisagePlugins==3.1.2',
-        'TraitsBackendQt==3.3.0',
-#        'numpy>=1.2.1', # can't be easy_installed
-        'Mayavi==3.3.1',
-        'configobj==4.7.2',
-        'tables==2.1.2',
-        'which==1.1.0',
-        'winpexpect==1.2',
-        'pexpect==2.4',
-#        'Matplotlib==0.99.1', # can't be easy_installed either
-    ],
-
+    # PyPI metadata
     name='InfobioticsDashboard',
     author='Jonathan Blakes',
     author_email='jvb@cs.nott.ac.uk',
@@ -58,4 +75,34 @@ setup(
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Text Editors :: Integrated Development Environments (IDE)',          
     ],
+
+    packages=find_packages(
+        exclude=[
+            "*.tests", "*.tests.*", "tests.*", "tests", # http://packages.python.org/distribute/setuptools.html#using-find-packages
+            'expect',
+        ]
+    ),
+
+    install_requires = install_requires + [
+        'EnthoughtBase>=3.0.4',
+        'AppTools>=3.3.1',
+        'Traits>=3.3.0',
+        'EnvisageCore>=3.1.2',
+        'TraitsGUI>=3.3.0',
+        'EnvisagePlugins>=3.1.2',
+        'TraitsBackendQt>=3.3.0',
+        'Mayavi>=3.3.1',
+        'configobj',#==4.7.2',
+        'tables>=2.1.2',
+
+#        'which==1.1.0', # in infobiotics.thirdparty
+
+#        'winpexpect==1.2', # done outside setup()
+#        'pexpect==2.4', # done outside setup()
+
+#        'numpy>=1.2.1', # can't be easy_installed
+#        'Matplotlib==0.99.1', # can't be easy_installed either
+    ],
+
+    **extra_options # http://svn.pythonmac.org/py2app/py2app/trunk/doc/index.html#cross-platform
 )
