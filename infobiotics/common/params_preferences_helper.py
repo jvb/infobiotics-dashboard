65431994319afdbe833f29d0b22c20ff6d16d1d5
jvb <devnull@localhost>
2010-05-19 15:19:27 +0100

    Removed executable=True for '_params_program' in ParamsPreferencesHelper

--- infobiotics/common/params_preferences_helper.py
+++ infobiotics/common/params_preferences_helper.py
@@ -3,5 +3,5 @@ from infobiotics.commons.traits.api import RelativeFile, RelativeDirectory
 
 class ParamsPreferencesHelper(PreferencesHelper):
     ''' Traits-based type checking and conversion from ini format files.'''
-    _params_program = RelativeFile(absolute=True, executable=True)#, auto_set=True
-    _cwd = RelativeDirectory(absolute=True)#, exists=True#, auto_set=True 
+    _params_program = RelativeFile(absolute=True)#, executable=True)#, auto_set=True)
+    _cwd = RelativeDirectory(absolute=True)#, exists=True)#, auto_set=True) 
