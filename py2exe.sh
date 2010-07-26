3d0ad3a819cbf5047eeb855f39c5f99fbb0ba895
jvb <devnull@localhost>
2010-07-26 15:09:08 +0100

    Added windows_missing_dlls.txt back to MANIFEST.in for py2exe.sh

--- py2exe.sh
+++ py2exe.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+
 export http_proxy=http://wwwcache-20.cs.nott.ac.uk:3128
 export https_proxy=https://wwwcache-20.cs.nott.ac.uk:3128
-bash clean.sh
+
 export ETS_TOOLKIT=qt4
+
+bash clean.sh
+
 python setup.py py2exe
-for i in $(cat windows_xp_missing_dlls.txt | cut -f 3 -d ' ' | sed 's/\.dll.* $/\.dll/g' | sed 's/\.DLL.*$/\.DLL/g' | sed 's/D:/C:/g') ; do cp $i dist/ ; done
-mkdir -p dist/enthought/tvtk/tvtk_classes
+
 current_drive=`python -c 'import os, sys; sys.stdout.write(os.getcwd()[0].lower())'`
+
+mkdir -p dist/enthought/tvtk/tvtk_classes
+unzip -q -d dist/enthought/tvtk/tvtk_classes /cygdrive/$current_drive/Python26/Lib/site-packages/enthought/tvtk/tvtk_classes.zip
+
+#TODO mv windows_xp_missing_dlls.txt HERE
+for i in $(cat windows_xp_missing_dlls.txt | cut -f 3 -d ' ' | sed 's/\.dll.* $/\.dll/g' | sed 's/\.DLL.*$/\.DLL/g' | sed 's/D:/C:/g') ; do cp $i dist/ ; done
+
 cp -r /cygdrive/$current_drive/Python26/Scripts/*.dll dist/
 #ls dist/*.dll
-unzip -q -d dist/enthought/tvtk/tvtk_classes /cygdrive/$current_drive/Python26/Lib/site-packages/enthought/tvtk/tvtk_classes.zip
+
 #rm -rf build
+
 #dist/infobiotics-dashboard.exe
 #cat dist/infobiotics-dashboard.exe.log
 #cat dist/pexpect_error.txt
