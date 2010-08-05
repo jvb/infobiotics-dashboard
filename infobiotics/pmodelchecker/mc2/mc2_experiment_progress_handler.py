5f1812ff61c68209a81bc327f82ce30a75e5ce18
jvb <devnull@localhost>
2010-08-05 15:45:40 +0100

    Stopped MC2 experiment spouting nonsense.

--- infobiotics/pmodelchecker/mc2/mc2_experiment_progress_handler.py
+++ infobiotics/pmodelchecker/mc2/mc2_experiment_progress_handler.py
@@ -2,11 +2,7 @@ from infobiotics.core.experiment_progress_handler import ExperimentProgressHandl
 from enthought.traits.api import on_trait_change
 
 class MC2ExperimentProgressHandler(ExperimentProgressHandler):
-
-    @on_trait_change('model.max_simulation, model.simulation')
-    def update_progress(self):
-        self.progress = int((100 / self.max_simulation) * self.simulation) 
-
+    pass
 #    @on_trait_change('model.max_simulation, model.simulation')
-#    def update_message(self):
-#        return 'MC2ExperimentProgressHandler._get_status()'
+#    def update_progress(self):
+#        self.progress = int((100 / self.max_simulation) * self.simulation) 
