8bf8f0e4d7e81d04424cd2890ab7d0c219dfbe97
jvb <devnull@localhost>
2010-08-05 17:47:46 +0100

    Removed pointless logging.

--- infobiotics/dashboard/plugins/text_editor/actions.py
+++ infobiotics/dashboard/plugins/text_editor/actions.py
@@ -4,19 +4,14 @@ from enthought.io.api import File
 from editor.text_editor import TextEditor
 from enthought.pyface.api import FileDialog, OK
 
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
 class NewFileAction(Action):
     ''' Open a new file in the text editor. '''
     tooltip = "Create a new file for editing"
     description = "Create a new file for editing"
 
-    window = Any() # The WorkbenchWindow the action is attached to.
+#    window = Any() # The WorkbenchWindow the action is attached to.
 
     def perform(self, event=None):
-        logger.info('NewFileAction.perform()')
         self.window.workbench.edit(File(''), kind=TextEditor,
             use_existing=False)
 
@@ -26,10 +21,7 @@ class OpenFileAction(Action):
 #    tooltip = "Open a file for editing"
     description = "Open a file for editing"
 
-    #TODO why doesn't this action need a window trait? 
-
     def perform(self, event=None):
-        logger.info('OpenFileAction.perform()')
         dialog = FileDialog(parent=self.window.control,
             title='Open File')
         if dialog.open() == OK:
@@ -49,7 +41,6 @@ class SaveFileAction(Action):
         return False
 
     def perform(self, event=None):
-        logger.info('SaveFileAction.perform()')
         self.window.active_editor.save()
 
 
@@ -66,5 +57,4 @@ class SaveAsFileAction(Action):
         return False
 
     def perform(self, event=None):
-        logger.info('SaveAsFileAction.perform()')
         self.window.active_editor.save_as()
