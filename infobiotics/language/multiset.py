4e4352b558fa6575570e1315e59873dcb24d94a5
jvb <devnull@localhost>
2010-11-22 10:05:07 +0000

    Add cardinality(), multiplicity() and __len__() methods to multiset.

--- infobiotics/language/multiset.py
+++ infobiotics/language/multiset.py
@@ -4,3 +4,42 @@ class multiset(Counter):
     def __str__(self):
         ''' Returns 'a + b + b' for {'a':1,'b':2}. '''
         return ' + '.join([k for k, v in self.items() for _ in range(v)])
+
+    def __len__(self):
+        '''
+        
+        >>> m = multiset({'a':1, 'b':2})
+        >>> len(m)
+        2
+        
+        '''
+        return super(multiset, self).__len__()
+
+    def cardinality(self):
+        ''' The total number of elements in a multiset, including repeated memberships, is the cardinality of the multiset. 
+        
+        >>> m = multiset({'a':1, 'b':2})
+        >>> m.cardinality()
+        3
+        
+        '''
+        return len(list(self.elements()))
+    
+    def multiplicity(self, key):
+        '''
+        
+        >>> m = multiset({'a':1, 'b':2})
+        >>> m.multiplicity('a')
+        1
+        >>> m.multiplicity('b')
+        2
+        >>> m.multiplicity('c')
+        0
+        
+        '''
+        return self[key]
+
+
+if __name__ == '__main__':
+    import doctest
+    print doctest.testmod()
