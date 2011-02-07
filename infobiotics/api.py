a9455bfd60e00f134af40068c02364c3263d1c6b
jvb <devnull@localhost>
2011-02-07 17:22:00 +0000

    Introduced alternative api at infobiotics.experiments

--- infobiotics/api.py
+++ infobiotics/api.py
@@ -1,8 +1,5 @@
 from preferences import preferences #TODO use config.py singleton instead?
 
-from mcss.api import McssExperiment
-
-from pmodelchecker.mc2.api import MC2Experiment
-from pmodelchecker.prism.api import PRISMExperiment
-
-from poptimizer.api import POptimizerExperiment
+from infobiotics.mcss.api import McssExperiment
+from infobiotics.pmodelchecker.api import MC2Experiment, PRISMExperiment 
+from infobiotics.poptimizer.api import POptimizerExperiment
---
+++ infobiotics/experiments.py
@@ -0,0 +1,3 @@
+from infobiotics.mcss.api import McssExperiment as mcss
+from infobiotics.pmodelchecker.api import MC2Experiment as mc2, PRISMExperiment as prism 
+from infobiotics.poptimizer.api import POptimizerExperiment as poptimizer
