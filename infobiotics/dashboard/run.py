78afeea24fa01060e9d3ff31a37cb1356c12057c
jvb <devnull@localhost>
2011-02-07 17:22:49 +0000

    Neater plugin creation - can override through parameter

--- infobiotics/dashboard/run.py
+++ infobiotics/dashboard/run.py
@@ -1,90 +1,74 @@
-# set process title and TraitsUI backend if run directly
+# if run directly
 if __name__ == '__main__':
+    
+    # set process title
     import setproctitle
     setproctitle.setproctitle('Infobiotics Dashboard')
+    
+    # set TraitsUI backend
     from enthought.etsconfig.api import ETSConfig
     ETSConfig.toolkit = 'qt4'
 
 # fixes 'no handlers could be found for logger "enthought.envisage.plugin"'
-import logging
-logger = logging.getLogger("enthought.envisage.plugin")
-logger.addHandler(logging.StreamHandler())
-#TODO establish good logging practice 
+import logging, os
+logger = logging.getLogger('enthought.envisage.plugin')
+logger.addHandler(logging.StreamHandler(os.devnull))
 
 # import plugins
 from enthought.envisage.core_plugin import CorePlugin
 from enthought.envisage.ui.workbench.workbench_plugin import WorkbenchPlugin
-
 #from enthought.envisage.developer.developer_plugin import DeveloperPlugin
 #from enthought.envisage.developer.ui.developer_ui_plugin import DeveloperUIPlugin
-
-#from enthought.plugins.text_editor.text_editor_plugin import TextEditorPlugin
-from infobiotics.dashboard.plugins.text_editor.text_editor_plugin import TextEditorPlugin
-
 from enthought.plugins.python_shell.python_shell_plugin import PythonShellPlugin
 #from enthought.plugins.ipython_shell.ipython_shell_plugin import IPythonShellPlugin # IPythonShellPlugin is not supported by Qt backend, yet.
-
+from infobiotics.dashboard.plugins.text_editor.text_editor_plugin import TextEditorPlugin #from enthought.plugins.text_editor.text_editor_plugin import TextEditorPlugin
 #from enthought.envisage.ui.single_project.project_plugin import ProjectPlugin #TODO
+from infobiotics.dashboard.plugins.core.ui_plugin import CoreUIPlugin
+#from infobiotics.dashboard.plugins.example.ui_plugin import ExampleUIPlugin
+#from infobiotics.dashboard.plugins.unified_open_action.unified_open_action_ui_plugin import UnifiedOpenActionUIPlugin
+#from infobiotics.dashboard.plugins.bnf.ui_plugin import BNFUIPlugin
+from infobiotics.dashboard.plugins.mcss.ui_plugin import McssUIPlugin
+from infobiotics.dashboard.plugins.simulator_results.ui_plugin import SimulatorResultsUIPlugin
+from infobiotics.dashboard.plugins.pmodelchecker.ui_plugin import PModelCheckerUIPlugin
+from infobiotics.dashboard.plugins.poptimizer.ui_plugin import POptimizerUIPlugin
 
 from infobiotics.dashboard.app import InfobioticsDashboardWorkbenchApplication
 
-from infobiotics.dashboard.plugins.core.ui_plugin import CoreUIPlugin
+def workbench_plugin_factory():
+    '''Creates a WorkbenchPlugin that doesn't prompt on exit by default.'''
+    from infobiotics.api import preferences
+    preferences.set('default/enthought.envisage.ui.workbench.prompt_on_exit', False) # previously done in infobiotics.preferences
+    # use our preferences instead of the pkgfile://enthought.envisage.ui.workbench/preferences.ini
+    return WorkbenchPlugin(my_preferences=['file://%s' % preferences.filename]) # need 'file://' because this later gets split on '://' 
 
-#from infobiotics.dashboard.plugins.unified_open_action.unified_open_action_ui_plugin import UnifiedOpenActionUIPlugin
+plugin_factories = [
+    CorePlugin,
+    workbench_plugin_factory,
+#    DeveloperPlugin,
+#    DeveloperUIPlugin,
+    PythonShellPlugin,
+#    IPythonShellPlugin,
+    TextEditorPlugin,
+#    ProjectPlugin, #TODO
+    CoreUIPlugin,
+#    UnifiedOpenActionUIPlugin,
+#    BNFUIPlugin,
+    McssUIPlugin,
+    SimulatorResultsUIPlugin,
+    PModelCheckerUIPlugin,
+    POptimizerUIPlugin,
+]
 
-from infobiotics.dashboard.plugins.mcss.ui_plugin import McssUIPlugin
-from infobiotics.dashboard.plugins.simulator_results.ui_plugin import SimulatorResultsUIPlugin
-
-from infobiotics.dashboard.plugins.pmodelchecker.ui_plugin import PModelCheckerUIPlugin
-
-from infobiotics.dashboard.plugins.poptimizer.ui_plugin import POptimizerUIPlugin
-
-#from infobiotics.dashboard.plugins.bnf.ui_plugin import BNFUIPlugin
-
-#from infobiotics.dashboard.plugins.example.ui_plugin import ExampleUIPlugin
-#TODO optimise plugin loading
-
-def main():
+def main(plugin_factories=plugin_factories):
     '''Main entry point for Infobiotics Dashboard.
 
     Creates the Workbench Application from a collection of plugins.
     '''
-    # don't prompt on exit by default
-    from infobiotics.api import preferences
-    preferences.set('default/enthought.envisage.ui.workbench.prompt_on_exit', False) # done in infobiotics.preferences
-    # use our preferences instead of the pkgfile://enthought.envisage.ui.workbench/preferences.ini
-    workbench_plugin = WorkbenchPlugin(my_preferences=['file://%s' % preferences.filename]) # splits on '://' 
 
     # create application from plugins
     application = InfobioticsDashboardWorkbenchApplication(
-        
-        # create plugins
-        plugins=[
-            CorePlugin(),
-            workbench_plugin,
-
-#            DeveloperPlugin(),
-#            DeveloperUIPlugin(),
-            PythonShellPlugin(),
-#            IPythonShellPlugin(),
-            TextEditorPlugin(),
-
-#            ProjectPlugin(), #TODO
-
-            CoreUIPlugin(),
-
-#            UnifiedOpenActionUIPlugin(),
-
-#            BNFUIPlugin(),
-
-            McssUIPlugin(),
-            SimulatorResultsUIPlugin(),
-
-            PModelCheckerUIPlugin(),
-            
-            POptimizerUIPlugin(),
-
-        ]
+        # create plugins by calling each plugin factory
+        plugins=[plugin_factory() for plugin_factory in plugin_factories],
     )
 
 #    window = application.active_window
