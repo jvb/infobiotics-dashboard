0388f91cdbcc195dff3e87dfe55667866d2562d3
jvb <devnull@localhost>
2011-08-12 13:38:50 +0100

    Interpolation of surfaces in space and time using scipy

--- infobiotics/mcss/results/combined_surfaces.py
+++ infobiotics/mcss/results/combined_surfaces.py
@@ -21,13 +21,15 @@ class Surfaces(HasTraits):
     def __init__(self, arrays, **traits):
         HasTraits.__init__(self, **traits)
         self.arrays = arrays
-        zmax = np.max(self.arrays)
-        self.warp_scale = (1 / zmax) * 10
+#        zmax = np.max(self.arrays)
+#        self.warp_scale = (1 / zmax) * 10
 
-    def surf_default(self, arrayindex, **kwargs):
-        surf = mlab.surf(self.arrays[arrayindex, :,:,0], 
-            warp_scale=self.warp_scale,
-            **kwargs)
+    def surf_default(self, arrayindex):
+        surf = mlab.surf(
+            self.arrays[arrayindex, :,:,0], 
+#            warp_scale=self.warp_scale
+            warp_scale=(1 / np.max(self.arrays[arrayindex])) * 40
+        )
         
         # Retrieve the LUT of the surf object.
         lut = surf.module_manager.scalar_lut_manager.lut.table.to_array()
@@ -40,14 +42,16 @@ class Surfaces(HasTraits):
 #        print lut.shape # (256, 4)
         if arrayindex == 1:
             lut[:, 0] = np.linspace(0, 255, 256) # red
-            lut[:, 1] = np.linspace(0, 0, 256) # alpha
+            lut[:, 1] = np.linspace(0, 0, 256) # green
             lut[:, 2] = np.linspace(255, 0, 256) # blue
-#            lut[:, 3] = np.linspace(0, 255, 256) # alpha
+            lut[:, 3] = np.linspace(255, 255, 256) # alpha
+#            lut[:, 3] = np.linspace(127, 127, 256) # alpha
         else:
             lut[:, 0] = np.linspace(0, 0, 256) # red
-            lut[:, 1] = np.linspace(0, 255, 256) # alpha
+            lut[:, 1] = np.linspace(0, 255, 256) # green
             lut[:, 2] = np.linspace(255, 0, 256) # blue
-#            lut[:, 3] = np.linspace(0, 255, 256) # alpha
+            lut[:, 3] = np.linspace(255, 255, 256) # alpha
+#            lut[:, 3] = np.linspace(127, 127, 256) # alpha
             
         
         # and finally we put this LUT back in the surface object. We could have
@@ -58,19 +62,17 @@ class Surfaces(HasTraits):
         mlab.draw()
         
         self.scene.scene_editor.isometric_view() #WARNING removing this line causes the surface not to display and the axes to rotate 90 degrees vertically!
+        self.scene.camera.azimuth(90)
         return surf
     
     @on_trait_change('scene.activated')
     def create_pipeline(self):
-        kwargs = dict(
-            #TODO colours
-        )
         for i in range(len(self.arrays)): 
-            self.surfaces.append(self.surf_default(i), **kwargs)
+            self.surfaces.append(self.surf_default(i))
     
     play = Button
     def _play_fired(self):
-        for i in range(len(self.arrays[0, :,:])):
+        for i in range(arrays.shape[-1]-1):
             for surfaceindex in range(len(self.surfaces)):
                 GUI.invoke_later(self.set_zindex, i, surfaceindex)
 
@@ -95,7 +97,29 @@ class Surfaces(HasTraits):
 npzfile = np.load('/home/jvb/Desktop/ThreeLayers.h5_surfaces.npz')
 FP1 = npzfile['FP1']
 FP2 = npzfile['FP2']
+FP1 = FP1[46:86,1:-1,:]#-120:] # crop to Central cells only
+FP2 = FP2[46:86,1:-1,:]#-120:] # crop to Central cells only
+#print FP1.shape, np.max(FP1)
+#print FP2.shape, np.max(FP2)
+#print
+from interpolation import interpolatexy, interpolatet, interpolate
+tmultiplier = 5
+xymultiplier = 3
+#FP1 = interpolatet(FP1, tmultiplier)
+#FP2 = interpolatet(FP2, tmultiplier)
+##print FP1.shape, np.max(FP1)
+##print FP2.shape, np.max(FP2)
+##print
+#FP1 = interpolatexy(FP1, xymultiplier)
+#FP2 = interpolatexy(FP2, xymultiplier)
+##print FP1.shape, np.max(FP1)
+##print FP2.shape, np.max(FP2)
+##print
+FP1 = interpolate(FP1, xymultiplier, tmultiplier)
+FP2 = interpolate(FP2, xymultiplier, tmultiplier)
 arrays = np.array([FP1, FP2])    
+#arrays = np.array([FP1])
+#arrays = np.array([FP2])
 surfaces = Surfaces(arrays)
     
 if __name__ == '__main__':
