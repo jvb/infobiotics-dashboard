1e456370f414cc35194cd65791029a52f4cf4bc9
jvb <devnull@localhost>
2011-08-13 20:22:07 +0100

    Not quite there

--- infobiotics/mcss/results/histograms.py
+++ infobiotics/mcss/results/histograms.py
@@ -1,4 +1,7 @@
-import infobiotics
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    import infobiotics
 from enthought.traits.api import *
 from enthought.traits.ui.api import *
 
@@ -14,44 +17,76 @@ from matplotlib.axes import Axes#, Subplot as AxesSubplot
 
 class Histograms(HasTraits):
     
-    bins = Int(10)
     data = Enum(['Compartments', 'Runs'])
     sum_species = Bool(False)
-#    quanitities_display_units
-
-    from_timepoint_index = Int
-    
-    def _from_timepoint_index(self):
-        return len(self.results.timepoints) - 1
-    
-    to_timepoint_index = Int # 3D only
+    bins = Range(2,100,10)
+#    quanitities_display_units = Enum(...) #TODO
 
     results = Instance(McssResults)
-    amounts = Property(Array, depends_on='data')
 
+    min_timepoint_index = Int#(0)
+    max_timepoint_index = Int
+    from_timepoint_index = Range('min_timepoint_index', 'max_timepoint_index')#'to_timepoint_index') # infinite recursion
+    to_timepoint_index = Range('from_timepoint_index', 'max_timepoint_index') # 3D only
+
+    amounts = Property(Array, depends_on='data')
+    species_amounts_index = Property(Int, depends_on='data')
 #    scene = Instance(MlabSceneModel)
+
+
+    def _data_default(self):
+        if self.results.num_selected_compartments > 1:
+            return 'Compartments'
+        else:
+            return 'Runs'
+
+    def _min_timepoint_index_default(self):
+        return 0
+
+    def _max_timepoint_index_default(self):
+        return len(self.results.timepoints) - 1
+
+    @cached_property
+    def _get_amounts(self):
+        if self.data == 'Compartments':
+            return self.results.functions_of_amounts_over_runs(mean)[0] # (species, compartments, timepoints)
+        else: # self.data == 'runs'
+            return mean(self.results.amounts(), self.results.amounts_axes.index('compartments')) # (runs, species, timepoints)
+    
+    @cached_property
+    def _get_species_amounts_index(self):
+        if self.data == 'Compartments':
+            return 0 # (species, compartments, timepoints)
+        else: # self.data == 'runs'
+            return 1 # (runs, species, timepoints)
+
     
     @classmethod
     def fromfile(cls, file):
         return cls(results=McssResults(file))
 
     def __init__(self, results, **traits):
+        assert results.num_selected_compartments > 1 or results.num_selected_runs > 1 
         HasTraits.__init__(self, results=results, **traits)
+        self.max_timepoint_index = len(self.results.timepoints) - 1
+        self.from_timepoint_index = self.max_timepoint_index / 2
+        self.update()
 
-    @cached_property
-    def _get_amounts(self):
-        if self.data == 'compartments':
-            return self.results.functions_of_amounts_over_runs(mean)[0] # (species, compartments, timepoints)
-        else: # self.data == 'runs'
-            return mean(self.results.amounts(), self.results.amounts_axes.index('compartments')) # (runs, species, timepoints)
-            
-    def update_plot(self, data):
+    def update_plot(self, data, range):
+        print data, data.shape
         self._figure.clear()
         axes = self._figure.add_subplot(1,1,1)
         axes.grid(True)
-        hist, bins, patches = axes.hist(data, self.bins)
-        self._figure.canvas.draw()
-        return axes        
+        label = [species.name for species in self.results.species]
+        hist, bins, patches = axes.hist(data, self.bins, label=label, 
+            range=range, 
+#           histtype='bar', alpha=0.5, 
+            histtype='step', alpha=1,
+        )
+        axes.legend()
+        if self._figure.canvas is not None:
+            self._figure.canvas.draw()
+        return axes
             
     @on_trait_change('bins, data, sum_species, from_timepoint_index, to_timepoint_index')
     def update(self):
@@ -71,32 +106,28 @@ class Histograms(HasTraits):
             transposed relative to the list form.
         '''
         # 2D
-        if self.data == 'compartments':
+        if self.data == 'Compartments':
             if self.sum_species:
                 sum_mean_amounts_over_runs_over_species = sum(self.amounts, 0)
                 data = sum_mean_amounts_over_runs_over_species[:, self.from_timepoint_index]
-                axes = self.update_plot(data)
+#                axes = self.update_plot(data)
             else:
-                numspecies = self.amounts.shape[0]
-#                for si in xrange(numspecies):
-#                    data = self.amounts[si, :, self.from_timepoint_index]
-#                data = self.amounts[:, :, self.from_timepoint_index]
-                data = self.amounts[:, :, self.from_timepoint_index].T
-                axes = self.update_plot(data)
+#                for si in xrange(self.amounts.shape[0]):
+#                    data = self.amounts[si, :, self.from_timepoint_index].T
+                data = self.amounts[:, :, self.from_timepoint_index].T # transpose because species and compartments are the wrong way round for axes.hist
+#                axes = self.update_plot(data)
                  
         else: # self.data == 'runs'
             if self.sum_species:
                 sum_mean_amounts_over_compartments_over_species = sum(self.amounts, 1)
                 data = sum_mean_amounts_over_compartments_over_species[:, self.from_timepoint_index]
-                axes = self.update_plot(data)
-            
+#                axes = self.update_plot(data)
             else:
-#                for si in xrange(numspecies):
+#                for si in xrange(self.amounts.shape[1]):
 #                    data = self.amounts[:, si, self.from_timepoint_index]
-#                data = self.amounts[:, :, self.from_timepoint_index].T # transpose because runs and species are the wrong way round
                 data = self.amounts[:, :, self.from_timepoint_index]
-                axes = self.update_plot(data)
-    
+#                axes = self.update_plot(data)
+        axes = self.update_plot(data)
     
     _figure = Instance(Figure)
     
@@ -113,11 +144,14 @@ class Histograms(HasTraits):
                 ),
             ),
             HGroup(
-                'bins',
-                'data',
+                Item('data', 
+#                    label='',
+                    enabled_when="object.results.num_selected_compartments > 1 and object.results.num_selected_runs > 1"    
+                ),
                 'sum_species',
-                'from_timepoint_index',
+                'bins',
             ),
+            Item('from_timepoint_index', label='Timepoint'),
             width=800, height=600,
             resizable=True,
             title='TODO',#self.window_title
@@ -127,8 +161,9 @@ class Histograms(HasTraits):
 
 
 def test():
-#    results = McssResults('../../../examples/mcss/models/module1.h5')
-    Histograms.fromfile('../../../examples/mcss/models/module1.h5').configure_traits()
+    Histograms.fromfile('../../../examples/tutorial-autoregulation/autoregulation_simulation.h5').configure_traits()
+
 
 if __name__ == '__main__':
-    test()    
+    test()
+    
