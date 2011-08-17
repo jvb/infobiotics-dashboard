65c8aa75ac6082d4c61f7c134b45bf19d89c09b3
jvb <devnull@localhost>
2011-08-17 17:33:01 +0100

    Improved ListWidget but not catching items added by creating QListWidgetItems whose parents are ListWidget. Need to remember original item order *without* self._items!

--- infobiotics/commons/pyqt4/list_widget.py
+++ infobiotics/commons/pyqt4/list_widget.py
@@ -1,9 +1,17 @@
-from PyQt4.QtGui import QListWidget, QAbstractItemView
+from PyQt4.QtGui import QListWidget, QAbstractItemView, QListWidgetItem
 from PyQt4.QtCore import SIGNAL, pyqtSignal, Qt, SLOT
-from infobiotics.commons.pyqt4.actions import add_actions, create_action
+from infobiotics.commons.pyqt4.actions import add_actions, create_action, QActionGroup,\
+    create_action_group, separator
+from infobiotics.commons.qt4 import disable_widgets, enable_widgets
+
 import operator
 import re
 
+def createQListWidgetItem(item):
+    '''Wraps item strings in QListWidgetItems so they can be returned.'''
+    return QListWidgetItem(item) if not isinstance(item, QListWidgetItem) else item 
+
+        
 class ListWidget(QListWidget):
     ''' QListWidget that preserves original ordering, with filtering, sorting, 
     and selection methods. ''' 
@@ -17,10 +25,13 @@ class ListWidget(QListWidget):
     def __init__(self, parent=None, use_built_in_context_menu=True):
         super(ListWidget, self).__init__(parent)
         
+        self._items = [] # holds original list of items
+        
         # selection
         self.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.selected = []
         self.connect(self, SIGNAL('itemSelectionChanged()'), self.remember_selection)
+        self.connect(self, SIGNAL('itemSelectionChanged()'), lambda: self.selectAllAction.setDisabled(self.selected_visible_count() == self.visible_count()))
         
         # filtering
         self._locked = False # necessary for safe filtering
@@ -31,16 +42,30 @@ class ListWidget(QListWidget):
         self.setSortingEnabled(False) # don't use QListWidget sort method
         self.is_sorted = False
 
-        if use_built_in_context_menu:
+        self.selectAllAction = create_action(self, 'Select &All', self.selectAll)
+        self.invertSelectionAction = create_action(self, '&Invert Selection', self.invert_selection) 
 
+        self.sortAscendingAction = create_action(self, '&Sort Ascending', lambda: self.sortItems(order=Qt.AscendingOrder))
+        self.sortDescendingAction = create_action(self, 'Sort &Descending', lambda: self.sortItems(order=Qt.DescendingOrder))
+        self.restoreOriginalOrderAction = create_action(self, '&Restore Original Order', lambda: self.sortItems(False))
+#        sortActionGroup = create_action_group(self)
+#        self.sortAscendingAction = create_action(sortActionGroup, '&Sort Ascending', lambda: self.sortItems(order=Qt.AscendingOrder))
+#        self.sortDescendingAction = create_action(sortActionGroup, 'Sort &Descending', lambda: self.sortItems(order=Qt.DescendingOrder))
+#        self.restoreOriginalOrderAction = create_action(sortActionGroup, '&Restore Original Order', lambda: self.sortItems(False))
+        
+        self.restoreOriginalOrderAction.setDisabled(True)
+
+        if use_built_in_context_menu:
             add_actions(
                 self,
                 [
-                    create_action(self, 'Select &All', self.selectAll),
-                    create_action(self, '&Invert Selection', self.invert_selection),
+                    self.selectAllAction,
+                    self.invertSelectionAction,
                     None,
-                    create_action(self, '&Sort Ascending', lambda: self.sortItems(order=Qt.AscendingOrder)),
-                    create_action(self, 'Sort &Descending', lambda: self.sortItems(order=Qt.DescendingOrder)),
+                    self.sortAscendingAction,
+                    self.sortDescendingAction,
+                    self.restoreOriginalOrderAction,
+#                    sortActionGroup,
                 ]
             )
             self.setContextMenuPolicy(Qt.ActionsContextMenu)
@@ -74,8 +99,10 @@ class ListWidget(QListWidget):
     def selectAll(self, checked=True): #TODO only_visible (True is same as QListWidget.selectAll!)
         self.disconnect(self, SIGNAL('itemSelectionChanged()'), self.remember_selection)
         if checked:
+#            self.selectAllAction.setEnabled(False)
             super(ListWidget, self).selectAll()
         else:
+#            self.selectAllAction.setEnabled(True)
             self.clearSelection()
             for item in self.selected:
                 item.setSelected(True) 
@@ -86,52 +113,64 @@ class ListWidget(QListWidget):
 #        self.addItems(['22', '23'])
 #        self.insertItems(2, ['22', '23'])
     
-    def invert_selection(self, only_visible=True): # False is same as QListWidget.selectAll()!
-#        for item in self.all_items():
-#            if item.isSelected():
-#                item.setSelected(False)
-#            else:
-#                item.setSelected(True)
+    def invert_selection(self, only_visible=True):
         if only_visible:
             [item.setSelected(not item.isSelected()) for item in self.visible_items()]
         else:   
-            [item.setSelected(not item.isSelected()) for item in self.all_items_generator()]
+            [item.setSelected(not item.isSelected()) for item in self.all_items()]
     
     def addItem(self, item, in_original_order=True):
+        print 'got here' # never gets called!
+        item = createQListWidgetItem(item)
         if in_original_order and self.is_sorted:
             self.sortItems(False, quiet=True)
-            super(ListWidget, self).addItem(item) # don't replace with self.insertItem(row=self.count())
-            self.sortItems(True, quiet=True)
-        else:
-            super(ListWidget, self).addItem(item)
+        super(ListWidget, self).addItem(item) # don't replace with self.insertItem(row=self.count()) # why not?
+        self._items.append(item)
+        if self.is_sorted:
+            self.sortItems(quiet=True)
+        return item
     
     def addItems(self, items, in_original_order=True):
-        for item in items:
-            self.addItem(item, in_original_order)
-    
+        return [self.addItem(item, in_original_order) for item in items]
+
     def insertItem(self, row, item, in_original_order=True):
-        if in_original_order and self.is_sorted: #TODO ambiguous?
+        item = createQListWidgetItem(item)
+        if in_original_order and self.is_sorted:
             self.sortItems(False, quiet=True)
-            super(ListWidget, self).insertItem(row, item)
-            self.sortItems(True, quiet=True)
-        else:
-            super(ListWidget, self).insertItem(row, item)
+        super(ListWidget, self).insertItem(row, item)
+        self._items.append(item)
+        if self.is_sorted:
+            self.sortItems(quiet=True)
+        return item
 
     def insertItems(self, row, items, in_original_order=True):
-        for item in items:
-            self.insertItem(row, item, in_original_order)
+        return [self.insertItem(row, item, in_original_order) for item in items]
 
-    def all_items_generator(self, in_original_order=True):
-        if in_original_order:
-            all_items_in_current_order = self.all_items_generator(in_original_order=False)
-            if not hasattr(self, '_items') or set(self._items) != set(all_items_in_current_order):
-                self._items = all_items_in_current_order
-            return (item for item in self._items)
-        else:
-            return [self.item(i) for i in range(self.count())]
+#    def remove(self, item):
+#        '''Removes item from ListWidget and deletes it. If you want to keep it use pop(item).'''
+#        row = self.row(item)
+#        self.takeItem(row)
+#        self._items.remove(item)
+#        del item
+#        
+#    def pop(self, item_or_row):
+#        '''Removes item and returns it.'''
+#        if not isinstance(item_or_row, int):
+#            # item_or_row is (hopefully) an item
+#            item_or_row = self.row(item_or_row)
+#        # item_or_row is the row
+#        item = self.takeItem(item_or_row)
+#        print item
+#        self._items.remove(item)
+#        return item
     
     def all_items(self, in_original_order=True):
-        return list(self.all_items_generator(in_original_order))
+        if in_original_order:
+            all_items = self._items#[:]
+        else:
+            all_items = [self.item(i) for i in range(self.count())]
+        print dict((self.row(item), str(item.text())) for item in all_items) #REMOVE
+        return all_items
 
     def filter(self, text, quiet=False):
         if self._locked == True:
@@ -173,33 +212,35 @@ class ListWidget(QListWidget):
 #            self.takeItem(0)
 
     def sortItems(self, checked=True, order=Qt.AscendingOrder, quiet=False, regex_functions=[]):
-
-
-        def fix_text_beginning_with_numbers(match, item):
-            return float(match.group())
-        regex_functions.append(('^\d+(\.?\d*)', fix_text_beginning_with_numbers))
+        '''
         
+        regex_functions is a list of tuples of regex match string and function  
+
+        Different to:
+            self.setSortingEnabled(True); 
+            super(ListWidget, self).sortItems()
+        Which doesn't sort strings beginning with numbers properly
+        
+        '''
         if order not in (Qt.AscendingOrder, Qt.DescendingOrder):
             raise ValueError('order not in (Qt.AscendingOrder, Qt.DescendingOrder)')
         
+        def fix_text_beginning_with_numbers(match, item):
+            return float(match.group())
+        regex_functions.insert(0, ('^\d+(\.?\d*)', fix_text_beginning_with_numbers))
+        
         self.disconnect(self, SIGNAL('itemSelectionChanged()'), self.remember_selection)
 
-        items = self.all_items() # must be done here so that self._items (with original ordering) is created via all_items_generator
-        
         if checked:
-#            self.setSortingEnabled(True)
-#            super(ListWidget, self).sortItems() # doesn't sort strings beginning with numbers properly
-            
             self.empty()
             
-            enumerated_items = [[i, item] for i, item in enumerate(items)]
+            enumerated_items = [[i, item] for i, item in enumerate(self.all_items())]
         
             for i, item in enumerated_items:
                 matched = False
                 for regex, function in regex_functions:
                     match = re.match(regex, item.text())
                     if match:
-#                        print regex
                         enumerated_items[i].append(function(match, item))
                         matched = True
                         break
@@ -217,13 +258,24 @@ class ListWidget(QListWidget):
             
             is_sorted = True
             
+            
+            if order == Qt.AscendingOrder:
+                self.restoreOriginalOrderAction.setDisabled(False)
+                self.sortAscendingAction.setDisabled(True)
+                self.sortDescendingAction.setDisabled(False)
+            else:
+                self.restoreOriginalOrderAction.setDisabled(False)
+                self.sortAscendingAction.setDisabled(False)
+                self.sortDescendingAction.setDisabled(True)
         else:
-#            self.setSortingEnabled(False)
-
+            self.restoreOriginalOrderAction.setDisabled(True)
+            self.sortAscendingAction.setDisabled(False)
+            self.sortDescendingAction.setDisabled(False)
+            
             self.empty()
             
             # add them back in the original order
-            for item in items: 
+            for item in self.all_items(in_original_order=True): 
                 super(ListWidget, self).addItem(item)
             
             is_sorted = False
@@ -236,22 +288,20 @@ class ListWidget(QListWidget):
         # refilter
         if self.filter_text != '':
             self.filter(self.filter_text)
-            
+
         if is_sorted != self.is_sorted:
-            self.is_sorted = is_sorted
             if not quiet:
+                self.is_sorted = is_sorted
                 self.is_sorted_changed.emit(self.is_sorted)
             
         self.connect(self, SIGNAL('itemSelectionChanged()'), self.remember_selection)
 
+
     def selected_count(self):
         return len(self.selectedItems())
     
-    def visible_items_generator(self): #TODO in_original_order
-        return (item for item in self.all_items() if not item.isHidden())
-    
-    def visible_items(self):
-        return list(self.visible_items_generator())
+    def visible_items(self, in_original_order=False):
+        return list(item for item in self.all_items(in_original_order) if not item.isHidden())
     
     def visible_count(self):
         return len(self.visible_items())
@@ -290,8 +340,8 @@ class ListWidget(QListWidget):
         return sort_check_box
     
     def connect_sort_check_box(self, sort_check_box):
-        self.connect(sort_check_box, SIGNAL('toggled(bool)'), self.sortItems)
-#        self.connect(self, SIGNAL('is_sorted_changed()'), sort_check_box, SLOT('toggled(bool)'))
+        self.connect(sort_check_box, SIGNAL('clicked(bool)'), self.sortItems)
+        self.connect(self, SIGNAL('is_sorted_changed(bool)'), sort_check_box, SLOT('setChecked(bool)'))
     
     def filter_line_edit_factory(self, parent=None):
         filter_line_edit = QLineEdit(parent)
@@ -308,63 +358,87 @@ class ListWidget(QListWidget):
 #        if int(split[0]) > 4 or (int(split[0]) == 4 and int(split[1]) >= 7): # doesn't work with PyQt 4.7
 #            filter_line_edit.setPlaceholderText('Filter')
 
+
+def check(item, checked=True):
+    item.setCheckState(Qt.Checked) if checked else item.setCheckState(Qt.Unchecked)
+    return item  
+    
+def uncheck(item):
+    return check(item, False)
+    
+
 class CheckBoxListWidget(ListWidget):
+    '''ListWidget with checkable items.'''
     
     def __init__(self, parent=None, use_built_in_context_menu=True):
         super(CheckBoxListWidget, self).__init__(parent, use_built_in_context_menu)
-        
         self.connect(self, SIGNAL('itemDoubleClicked(QListWidgetItem*)'), self.toggle_item_check_state)
         
-    def toggle_item_check_state(self, item):
-        if item.checkState() == Qt.Unchecked:
-            item.setCheckState(Qt.Checked)
-        else:
-            item.setCheckState(Qt.Unchecked)
+    def addItem(self, item, in_original_order=True, checked=False):
+        item = super(CheckBoxListWidget, self).addItem(item, in_original_order)
+        check(item, checked)
+        return item
     
-    def check_selected(self):
-        for item in self.selectedItems():
-            item.setCheckState(Qt.Checked)
+    def insertItem(self, row, item, in_original_order=True, checked=False):
+        item = super(CheckBoxListWidget, self).insertItem(row, item, in_original_order)
+        check(item, checked)
+        return item
+    
+    def toggle_item_check_state(self, item):
+        return check(item, item.checkState() == Qt.Unchecked)
+    
+    def check_selected(self, checked=True):
+        return [check(item, checked) for item in self.selectedItems()]
 
     def uncheck_selected(self):
-        for item in self.selectedItems():
-            item.setCheckState(Qt.Unchecked)
+        return self.check_selected(False)
     
-    def checked_items(self):
-        return [item for item in self.all_items() if item.checkState() == Qt.Checked]
+    def checked_items(self, only_visible=True):
+        if only_visible:
+            return [item for item in self.visible_items() if item.checkState() == Qt.Checked]
+        else:   
+            return [item for item in self.all_items() if item.checkState() == Qt.Checked]
+        
 
-    
-    
 
 if __name__ == '__main__':
     from PyQt4.QtGui import QApplication, QWidget, QVBoxLayout, QLineEdit, QCheckBox, QPushButton
     app = QApplication([])
     
-    list_widget = ListWidget()#CheckBoxListWidget()
-#    list_widget.addItems([str(i) for i in reversed(range(0, 15, 2))])
-#    list_widget.addItems(['1.1', '1.0a', '1.0b', '2', '12'])
-#    list_widget.addItems(['a', '1', '2', 'a(1,2)', 'a(1.2)', 'a(2,1)', '2,1'])
-    list_widget.addItems(['a(1,2)', '(0,2)e', 'b(1,1)', 'a(2,1)', '(99,100)', '(100,99)'])
+    lw = ListWidget()
+#    lw = CheckBoxListWidget()
+
+#    lw.addItems([str(i) for i in reversed(range(0, 15, 2))])
+#    lw.addItems(['1.1', '1.0a', '1.0b', '2', '12'])
+#    lw.addItems(['a', '1', '2', 'a(1,2)', 'a(1.2)', 'a(2,1)', '2,1'])
+    items = ['a(1,2)', '(0,2)e', 'b(1,1)', 'a(2,1)', '(99,100)', '(100,99)']
+#    lw.addItems(items)
+    for item in items:
+        QListWidgetItem(item, lw)
     
-    filter_line_edit = list_widget.filter_line_edit_factory()
     
-    all_selected_check_box = list_widget.all_selected_check_box_factory()    
+    l = QVBoxLayout()
+    l.addWidget(lw)
+    l.addWidget(lw.filter_line_edit_factory())
+    l.addWidget(lw.all_selected_check_box_factory())
+    l.addWidget(lw.invert_selection_push_button_factory('&Invert Selection'))
+    l.addWidget(lw.sort_check_box_factory())
     
-    invert_selection_push_button = list_widget.invert_selection_push_button_factory('&Invert Selection')
+    import random
+    button = QPushButton('add random')
+    button.connect(button, SIGNAL('clicked(bool)'), lambda: lw.addItem(random.choice(['x','y','z']), True))
+    l.addWidget(button)
+    button2 = QPushButton('remove random')
+    button2.connect(button2, SIGNAL('clicked(bool)'), lambda: lw.pop(random.randint(0, lw.count()-1)))
+    l.addWidget(button2)
     
-    sort_check_box = list_widget.sort_check_box_factory()
+    self = QWidget()
+    self.setLayout(l)
+    self.setGeometry(600, 400, 200, 480)
+    self.setWindowTitle(lw.__class__.__name__)
+    self.show()
     
-    layout = QVBoxLayout()
-    layout.addWidget(list_widget)
-    layout.addWidget(filter_line_edit)
-    layout.addWidget(all_selected_check_box)
-    layout.addWidget(invert_selection_push_button)
-    layout.addWidget(sort_check_box)
-    widget = QWidget()
-    widget.setLayout(layout)
-    widget.setGeometry(600, 300, 200, 480)
-    widget.show()
-    
-    list_widget.select(-2)
-    list_widget.select(9)
+#    lw.select(-2)
+#    lw.select(9)
     
     exit(app.exec_())
